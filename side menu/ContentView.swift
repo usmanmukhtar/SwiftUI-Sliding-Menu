87d0b94eb0e145c7ec1972c93073483356dbcd19
Usman Mukhtar <usmanmukhtar432@gmail.com>
2020-07-09 16:32:31 +0500

    Initial Commit

---
+++ side menu/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  side menu
+//
+//  Created by Usman Mukhtar on 09/07/2020.
+//  Copyright © 2020 Usman Mukhtar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
